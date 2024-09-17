092090a741f6ea706f02401f55b15fbef1441345
James <9022370@student.zadkine.nl>
2024-09-17 11:00:02 +0200

    test

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Book from './components/Book'
 
 function App() {
 
   return (
     <>
-     <h1>I HATE ZIGGERS</h1>
+     <h1>Amazon best seller:</h1>
+     <Book/>
     </>
   )
 }
